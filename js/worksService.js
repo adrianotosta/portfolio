efbcd531004e50a912f6fa2d79594c17fcee07bc
adrianotosta <adriano.tosta@gmail.com>
2015-01-25 18:25:29 -0200

    replace the data in array of the works

--- js/worksService.js
+++ js/worksService.js
@@ -4,35 +4,130 @@ works.service("worksListService", function(){
     this.workList = [
         {
             "id": 0,
-            "title": "title 0",
-            "client": "client 0",
-            "description": "text 0",
-            "role": "role 0",
-            "alt": "0eee",
-            "image": "work_0"
+            "title": "website",
+            "description": "This website was my first international project. It had been a web gallery of a Brazilian photographer who had lived in Barcelona.",
+            "client": "Petala Os",
+            "duty": "wireframe, html, css, jquery",
+            "thumbnail": "works\/mini_work_0.jpg",
+            "picture": "works\/work_0.jpg",
+            "status": "show"
         },
         {
             "id": 1,
-            "name": "title 1",
-            "description": "text 1",
-            "alt": "1"
+            "title": "website",
+            "description": "This project has been a donation to philantropic institution called Seicho-No-Ie, in Brazil.\r\n",
+            "client": "Seicho-No-Ie",
+            "duty": "wireframe, layout",
+            "thumbnail": "works\/mini_work_1.jpg",
+            "picture": "works\/work_1.jpg",
+            "status": "show"
         },
         {
             "id": 2,
-            "name": "title 2",
-            "description": "text 2",
-            "alt": "2"
+            "title": "branding",
+            "description": "This was a university project. I and my friends developed a new brand to CEDECA, it is philantropic institution which helps raped childrens.",
+            "client": "CEDECA",
+            "duty": "branding",
+            "thumbnail": "works\/mini_work_2.jpg",
+            "picture": "works\/work_2.png",
+            "status": "hidden"
         },
         {
             "id": 3,
-            "name": "title 3",
-            "description": "text 3",
-            "thumbnail": "3"
+            "title": "website",
+            "description": "This is template for martial arts dojo.",
+            "client": "",
+            "duty": "wireframe, layout",
+            "thumbnail": "works\/mini_work_3.jpg",
+            "picture": "works\/work_3.jpg",
+            "status": "show"
         }
+            ,
+//        {
+//            "id": 4,
+//            "title": "website",
+//            "description": "This is a template for a automotive store",
+//            "client": "",
+//            "duty": "wireframe, layout",
+//            "thumbnail": "works\/mini_work_4.jpg",
+//            "picture": "works\/work_4.jpg",
+//            "status": "show"
+//        },
+//        {
+//            "id": 5,
+//            "title": "website",
+//            "description": "This is a template for software store",
+//            "client": "",
+//            "duty": "wireframe, layout",
+//            "thumbnail": "works\/mini_work_5.jpg",
+//            "picture": "works\/work_5.jpg",
+//            "status": "show"
+//        },
+//        {
+//            "id": 6,
+//            "title": "website",
+//            "description": "This template was created for a bride magazine. ",
+//            "client": "",
+//            "duty": "wierframe, layout",
+//            "thumbnail": "works\/mini_work_6.jpg",
+//            "picture": "works\/work_6.jpg",
+//            "status": "show"
+//        },
+//        {
+//            "id": 7,
+//            "title": "website",
+//            "description": "This website was created for a company which sells and rents equipaments for civil construction industry.",
+//            "client": "Trajetto",
+//            "duty": "wireframe, layout, html, css, jquery",
+//            "thumbnail": "works\/mini_work_7.jpg",
+//            "picture": "works\/work_7.jpg",
+//            "status": "show"
+//        },
+//        {
+//            "id": 8,
+//            "title": "brand",
+//            "description": "I was responsible for creating a re-brand for iP3. This job included its products too",
+//            "client": "iP3",
+//            "duty": "brand",
+//            "thumbnail": "works\/mini_work_8.jpg",
+//            "picture": "works\/work_8.jpg",
+//            "status": "hidden"
+//        },
+//        {
+//            "id": 9,
+//            "title": "newsletter",
+//            "description": "This is a newsletter which invited all employee of iP3 to send a new ideia for a new version of Neo Cobran\u00e7a",
+//            "client": "iP3",
+//            "duty": "layout",
+//            "thumbnail": "works\/mini_work_9.jpg",
+//            "picture": "works\/work_9.jpg",
+//            "status": "hidden"
+//        },
+//        {
+//            "id": 10,
+//            "title": "newsletter",
+//            "description": "A newsletter invited all employee of iP3 for a Sao Joao`s party ",
+//            "client": "iP3",
+//            "duty": "layout",
+//            "thumbnail": "works\/mini_work_10.jpg",
+//            "picture": "works\/work_10.jpg",
+//            "status": "hidden"
+//        },
+//        {
+//            "id": 11,
+//            "title": "signal",
+//            "description": "A signal put in all bathroom warning all employee about consumption of water",
+//            "client": "iP3",
+//            "duty": "layout",
+//            "thumbnail": "works\/mini_work_11.jpg",
+//            "picture": "works\/work_11.jpg",
+//            "status": "hidden"
+//        }
     ];
     
     this.showWorkDetail = function(){
         var workDetail = document.getElementsByClassName("workDetail");
         workDetail[0].classList.toggle("ng-hide");
+        workDetail[0].classList.toggle("bounceIn");
     };
 });
