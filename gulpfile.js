57836cc6e1242211ba287d029ea8c562de140cbf
adrianotosta <adriano.tosta@gmail.com>
2015-03-29 19:06:09 -0300

    adding gulp plugin for minification and minificate of js and css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,18 @@
-var gulp   = require('gulp');
+var gulp      = require('gulp'),
+    minifyJs  = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css');
+
+gulp.task("minificaCSS", function(){
+    gulp.src("_site/css/main.css")
+        .pipe(minifyCss({keepBreaks: true}))
+        .pipe(gulp.dest("./images"));
+});
+
+gulp.task('minificaJS', function(){
+    gulp.src('_site/js/*.js')
+        .pipe(minifyJs())
+        .pipe(gulp.dest("./images"));
+});
 
 gulp.task('default', function(){
     console.log("aaaaa");
